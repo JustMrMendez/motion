558325929b083529aaf15a48b95a506c54c05015
Mr. Mendez <mrmendez@amvestor.com>
2023-09-06 12:40:39 -0400

    getting code block working

---
+++ src/lib/slides/Css.svelte
@@ -0,0 +1,26 @@
+<script>
+
+	import { Slide, Code } from '@components'
+	
+</script>
+
+<!-- Slide 1: Title Slide -->
+<Slide animate>
+	<!-- write the CSS IS AWESOME logo using tailwindcss -->
+	<div class="border-[5px] w-[400px] h-fit p-8 space-y-5 mx-auto">
+		<h1 class="text-8xl font-bold uppercase text-left">css is</h1>
+		<h1 class="text-8xl font-bold text-center uppercase">awesome</h1>
+	</div>
+</Slide>
+
+<!-- Slide 2: CSS -->
+<Slide animate>
+	<Code lang="svelte">
+		{`
+        <script>
+          let count = 0
+          $: double = count * 2
+        </script>
+      `}
+	</Code>
+</Slide>
--- src/slides.svelte
+++ src/slides.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-	import Html from '@lib/slides/Html.svelte'
-import Codespaces from './lib/slides/Codespaces.svelte';
-	import { Presentation, Slide } from '@components'
-	import Intro from '@lib/slides/Intro.svelte'
-	import Learnpack from '@lib/slides/Learnpack.svelte'
-	import Prework from '@lib/slides/Prework.svelte'
+	import { Presentation } from '@components'
+	import Css from '@lib/slides/Css.svelte'
 	// If you are using an SVG or other assets, import them here
 </script>
 
 <Presentation>
-	<Intro />
-	<Prework />
-	<Codespaces />
-	<Learnpack />
-	<Html />
+	<Css />
 </Presentation>
